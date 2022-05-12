fdebc59e2f5e5eda441d8e009a9f2350778a8a5b
tsaniamagistra <123210090@student.upnyk.ac.id>
2022-05-12 15:26:31 +0700

    menambahkan bagian pesanan

--- gudang-olshop.cpp
+++ gudang-olshop.cpp
@@ -6,9 +6,9 @@
 
 using namespace std;
 
-int menu(); void tambah(); void tampil(); void ubah(); void hapus();
+int menu(); void tambah(); void tampil(); void ubah(); void hapus(); void pesanan();
 
-struct gudangolshop
+struct databarang
 {
 	string nama;
 	string merk;
@@ -16,7 +16,7 @@ struct gudangolshop
 	int harga;
 };
 
-gudangolshop gd[100];
+databarang barang[100];
 
 int hitung = 0;
 
@@ -30,7 +30,9 @@ int main(){
             tampil();
         else if (select == 3)
             ubah();
-		else if (select == 4)
+        else if (select == 4)
+			pesanan();
+		else if (select == 5)
 			break;
 	}
 	return 0;
@@ -47,7 +49,8 @@ int menu()
 	cout << " [1] Tambahkan Produk\n";
 	cout << " [2] Tampilkan Produk\n";
 	cout << " [3] Ubah Produk\n";
-	cout << " [4] Keluar\n\n";
+	cout << " [4] Daftar Pesanan\n";
+	cout << " [5] Keluar\n\n";
 	cout << " Pilih	: ";
 
 	cin>>sel;
@@ -64,13 +67,13 @@ void tambah()
 	cout << "[==============================================================]\n\n";
 	cout << " Tambahkan Produk\n\n";
 	cout << " Nama Produk : ";
-	cin.ignore(); getline(cin, gd[hitung].nama);
+	cin.ignore(); getline(cin, barang[hitung].nama);
 	cout << " Merk Produk : ";
-	getline(cin, gd[hitung].merk);
+	getline(cin, barang[hitung].merk);
 	cout << " Stok Produk : ";
-	getline(cin, gd[hitung].stok);
+	getline(cin, barang[hitung].stok);
 	cout << " Harga Produk : ";
-	cin >> gd[hitung].harga;
+	cin >> barang[hitung].harga;
 
 	++hitung;
 }
@@ -93,10 +96,10 @@ void tampil()
 
     for(int i = 0; i < hitung; i++){
         cout << "|" << setw(4) << i+1;
-        cout << setw(15) << gd[i].nama;
-        cout << setw(15) << gd[i].merk;
-        cout << setw(15) << gd[i].stok;
-        cout << setw(7) << "Rp. " << gd[1].harga;
+        cout << setw(15) << barang[i].nama;
+        cout << setw(15) << barang[i].merk;
+        cout << setw(15) << barang[i].stok;
+        cout << setw(7) << "Rp. " << barang[1].harga;
         cout << setw(7) << "| "<< endl;
 		cout << "[==============================================================]\n";
         }
@@ -120,13 +123,13 @@ void ubah()
 	if (b < hitung)
 	{
 		cout << "\n Nama Produk : ";
-		getline(cin, gd[b].nama);
+		getline(cin, barang[b].nama);
 		cout << " Merk Produk : ";
-		getline(cin, gd[b].merk);
+		getline(cin, barang[b].merk);
 		cout << " Stok Produk : ";
-		getline(cin, gd[b].stok);
+		getline(cin, barang[b].stok);
 		cout << " Harga Produk (ecer) : ";
-		cin >> gd[b].harga;
+		cin >> barang[b].harga;
 	}
 
 	else
@@ -136,3 +139,12 @@ void ubah()
 	}
 }
 
+void pesanan()
+{
+	system("cls");
+    cout << "[==============================================================]\n";
+    cout << "|                      Gudang Online Shop                      |\n";
+    cout << "|                           Pesanan                            |\n";
+    cout << "[==============================================================]\n\n";
+	
+}
