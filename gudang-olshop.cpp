df7156063e3d2dbe58eb99bfc49fd2368511d99d
muhammadahnaf24 <muhammadahnav@gmail.com>
2022-05-21 13:52:20 +0700

    tambah ubah prd

--- gudang-olshop.cpp
+++ gudang-olshop.cpp
@@ -9,13 +9,20 @@ using namespace std;
 int menu(); int menuProduk(),menuPesanan();
 void tambahPrd(),tampilPrd(),ubahPrd(),hapusPrd();
 void tambahPsn(),tampilPsn(),ubahPsn();
-
+void header();
 struct dataProduk
 {
+	int no;
 	string nama;
 	string merk;
 	string stok;
 	int harga;
+	void writeToFile2(){
+		ofstream outfile;
+		outfile.open("dbProduk.txt", ios::app);
+		outfile << no << nama << endl << merk << endl << stok << endl << harga  << endl;
+		outfile.close();
+	}
 };
 dataProduk produk[100];
 
@@ -42,8 +49,8 @@ int main(){
 				else if (q == 2){
 					kembali = '0';
 					tampilPsn();
-					cout<<"input k untuk kembali : ";
-					cin>>kembali;
+					cout << "input k untuk kembali : ";
+					cin >> kembali;
 				}
 				else if (q == 3) ubahPsn();
 				else if (q == 4) break;
@@ -58,9 +65,7 @@ int menu()
 {
 	system("cls");
     int sel;
-	cout << "[==============================================================]\n";
-	cout << "|                      Gudang Online Shop                      |\n";
-	cout << "[==============================================================]\n\n";
+	header();
 	cout << " Menu Utama\n\n";
 	cout << " [1] Data Produk\n";
 	cout << " [2] Data Pesanan\n";
@@ -75,10 +80,7 @@ int menuProduk()
 {
 	system("cls");
     int sel;
-	cout << "[==============================================================]\n";
-	cout << "|                      Gudang Online Shop                      |\n";
-	cout << "|                            Produk                            |\n";
-	cout << "[==============================================================]\n\n";
+	header();
 	cout << " Menu Utama\n\n";
 	cout << " [1] Tambahkan Produk\n";
 	cout << " [2] Tampilkan Produk\n";
@@ -90,88 +92,148 @@ int menuProduk()
 	return sel;
 }
 
-void tambahPrd()
-{
+void tambahPrd(){
+	dataProduk produk;
 	system("cls");
-	cout << "[==============================================================]\n";
-	cout << "|                      Gudang Online Shop                      |\n";
-	cout << "|                        Tambah Produk                         |\n";
-	cout << "[==============================================================]\n\n";
-	cout << " Tambahkan Produk\n\n";
-	cout << " Nama Produk : ";
-	cin.ignore(); getline(cin, produk[hitung].nama);
-	cout << " Merk Produk : ";
-	getline(cin, produk[hitung].merk);
-	cout << " Stok Produk : ";
-	getline(cin, produk[hitung].stok);
-	cout << " Harga Produk : ";
-	cin >> produk[hitung].harga;
-
-	++hitung;
+	header();
+	cout << " Tambah Produk\n";
+	cout << " ---------------\n";
+	cout << " No	: "; cin >> produk.no;
+	cout << " Nama	: "; cin >> produk.nama;
+	cout << " Merk	: "; cin >> produk.merk;
+	cout << " Stok	: "; cin >> produk.stok;
+	cout << " Harga	: "; cin >> produk.harga;
+	produk.writeToFile2();
 }
 
 void tampilPrd()
 {
+	ifstream tampil;
+	int i = 0;
+
+	tampil.open("dbProduk.txt");
+
+	if(!tampil.fail()){
+		while(!tampil.eof()){
+			cout<<"bisa.";
+			tampil >> produk[i].no
+				>> produk[i].nama
+				>> produk[i].merk
+				>> produk[i].stok
+				>> produk[i].harga;
+			if(!tampil.eof()) i++;
+		}
+	}
     system("cls");
-    cout << "[==============================================================]\n";
-    cout << "|                      Gudang Online Shop                      |\n";
-    cout << "|                         Isi Produk                           |\n";
-    cout << "[==============================================================]\n\n";
-    cout << "[==============================================================]\n";
-	cout << setw(5) << "| No.";
+	header();
+    cout << "+==============================================================+\n";
+	cout << setw(4) << "| No";
 	cout << setw(15) << "Nama Produk";
 	cout << setw(15) << "Merk Produk";
 	cout << setw(15) << "Stok Produk";
-	cout << setw(11) << "Harga" <<"  | ";
+	cout << setw(11) << "Harga" <<"   | ";
 	cout << endl;
-    cout << "[==============================================================]\n";
+    cout << "+==============================================================+\n";
 
-    for(int i = 0; i < hitung; i++){
-        cout << "|" << setw(4) << i+1;
-        cout << setw(15) << produk[i].nama;
-        cout << setw(15) << produk[i].merk;
-        cout << setw(15) << produk[i].stok;
-        cout << setw(7) << "Rp. " << produk[1].harga;
-        cout << setw(7) << "| "<< endl;
-		cout << "[==============================================================]\n";
-        }
+	for(int k=0;k<i;k++){
+		cout << "| " 
+			<< setiosflags(ios::left) << setfill(' ') << setw(7) << produk[k].no
+			<< setiosflags(ios::left) << setfill(' ') << setw(15) << produk[k].nama
+			<< setiosflags(ios::left) << setfill(' ') << setw(18) << produk[k].merk
+			<< setiosflags(ios::left) << setfill(' ') << setw(12) << produk[k].stok
+			<< setiosflags(ios::left) << setfill(' ') << setw(9) << produk[k].harga << "|\n";
+	}
+	cout << "+==============================================================+\n";
+	tampil.close();
+
     system("pause");
 }
 
-void ubahPrd()
-{
-    int a,b;
-    system("cls");
-    cout << "[==============================================================]\n";
-    cout << "|                      Gudang Online Shop                      |\n";
-    cout << "|                         Ubah Produk                          |\n";
-    cout << "[==============================================================]\n\n";
-    cout << " Urutan produk yang ingin diedit	: ";
-	cin >> a;
-	cin.ignore(1, '\n');
+void ubahPrd(){
+	system("cls");
+	header();
 
-	b = a - 1;
-
-	if (b < hitung)
-	{
-		cout << "\n Nama Produk : ";
-		getline(cin, produk[b].nama);
-		cout << " Merk Produk : ";
-		getline(cin, produk[b].merk);
-		cout << " Stok Produk : ";
-		getline(cin, produk[b].stok);
-		cout << " Harga Produk : ";
-		cin >> produk[b].harga;
+	fstream data;
+	data.open("dbProduk.txt", ios::in);
+	data.seekg(0, ios::beg);
+	int i = 0;
+	while(!data.eof()){
+		data >> produk[i].no
+			>> produk[i].nama
+			>> produk[i].merk
+			>> produk[i].stok
+			>> produk[i].harga;
+		i++;
 	}
-
-	else
-	{
-		cout << "\n Produk tidak ditemukan" << endl;
-		system("pause");
+	data.close();
+	cout << " Ubah Produk\n\n";
+	cout << " Masukkan No Produk yang akan diubah: ";
+	int no;
+	cin >> no;
+	for(int k=0;k<i;k++){
+		if(produk[k].no == no){
+			cout << " Nama Produk Baru: ";
+			cin.ignore(); getline(cin, produk[k].nama);
+			cout << " Merk Produk Baru: ";
+			getline(cin, produk[k].merk);
+			cout << " Stok Produk Baru: ";
+			getline(cin, produk[k].stok);
+			cout << " Harga Produk Baru: ";
+			cin >> produk[k].harga;
+		}
 	}
+	data.open("dbProduk.txt", ios::out);
+	for(int k=0;k<i;k++){
+		data << produk[k].no << endl
+			<< produk[k].nama << endl
+			<< produk[k].merk << endl
+			<< produk[k].stok << endl
+			<< produk[k].harga << endl;
+	}
+	data.close();
+	cout << " Data Produk Berhasil Diubah\n";
 }
 
-void hapusPrd(){}
+void hapusPrd(){
+	system("cls");
+	header();
+	fstream data;
+	data.open("dbProduk.txt", ios::in);
+	data.seekg(0, ios::beg);
+	int i = 0;
+	while(!data.eof()){
+		data >> produk[i].no
+			>> produk[i].nama
+			>> produk[i].merk
+			>> produk[i].stok
+			>> produk[i].harga;
+		i++;
+	}
+	data.close();
+	cout << " Hapus Produk\n\n";
+	cout << " Masukkan No Produk yang akan dihapus: ";
+	int no;
+	cin >> no;
+	for(int k=0;k<i;k++){
+		if(produk[k].no == no){
+			produk[k].no = produk[i-1].no;
+			produk[k].nama = produk[i-1].nama;
+			produk[k].merk = produk[i-1].merk;
+			produk[k].stok = produk[i-1].stok;
+			produk[k].harga = produk[i-1].harga;
+		}
+	}
+	data.open("dbProduk.txt", ios::out);
+	for(int k=0;k<i-1;k++){
+		data << produk[k].no << endl
+			<< produk[k].nama << endl
+			<< produk[k].merk << endl
+			<< produk[k].stok << endl
+			<< produk[k].harga << endl;
+	}
+	data.close();
+	system("pause");
+}
 
 int menuPesanan()
 {
@@ -261,5 +323,12 @@ void tampilPsn(){
 	cout<<endl;
 	
 	fin.close();
+}
+
+void header(){
+	cout << "[==============================================================]\n";
+	cout << "|                      Gudang Online Shop                      |\n";
+	cout << "|                                                              |\n";
+	cout << "[==============================================================]\n\n";
 }
 void ubahPsn(){}
